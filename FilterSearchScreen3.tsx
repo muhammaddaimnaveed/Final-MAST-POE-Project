ce4f9d966349f06b4edc2bff0046dcff3c0216a8
Muhammad Naveed <mdaim1101@gmail.com>
2024-11-22 14:09:48 +0200

    This Screen consists of all the menu items for the Desert Course.

--- FilterSearchScreen3.tsx
+++ FilterSearchScreen3.tsx
@@ -8,6 +8,8 @@ interface Dish {
   description: string;
 }
 
+/*This Screen consists of all the menu items for the Desert Course. */
+
 const FilterSearchScreen3: React.FC<{ route: any; navigation: any }> = ({ route, navigation }) => {
   const [dishes, setDishes] = useState<Dish[]>([]);
   const [savedDishes, setSavedDishes] = useState<Dish[]>([]);
